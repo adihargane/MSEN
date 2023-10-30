93c4b50ea9a43a565814f37c91ea03602295b847
Aditya Hargane <aditya.hargane@sutradhar.co.in>
2023-10-30 11:49:06 +0530

    created new file for sequelize file

---
+++ src/config/sequelize.js
@@ -0,0 +1,33 @@
+const { Sequelize } = require("sequelize");
+const mysql2Connection = require("./db.js"); // Import your existing mysql2 connection
+require("dotenv").config();
+
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+testConnection();
+
+module.exports = sequelize;
